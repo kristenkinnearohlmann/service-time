735729e61842cb1e72352329ef751fac0a1a10a3
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-08-20 00:12:42 -0500

    Added Express code to serve index HTML file

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// This code is from first example site
 // base file - call into app dir
 
-require('./app/index')
+// require('./app/index')
+
+// This is from another tutorial
+const express = require('express')
+const app = express()
+const port = 3002
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname})
+})
+
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`)
+})
