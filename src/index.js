642a1b6c0d8b1e525d2b6a6c9127330a9319d7dd
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-09-14 22:28:59 -0500

    Organized index.js for work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('Hello world, is this thing on?')
+const init = () => {
+    helloWorld()
+    goodbyeWorld()
+}
+
+const helloWorld = () => {
+    console.log("Hello world, I'm starting up")
+}
+
+const goodbyeWorld = () => {
+    console.log("Goodbye world, I'm shutting down")
+}
+
+// start the application
+init()
